85981c3fba34f26c9b6513180fe0ac78e21301ec
abd-005 <abd.me7@gmail.com>
2025-09-18 20:49:24 +0600

    seventh commit

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,18 +6,32 @@
 //     )
 // }
 
-export default function ToDo({ task, isDone, duration }) {
-  if (isDone) {
-    return (
-      <p>
-        Done: {task} Duration: {duration}
-      </p>
-    );
-  } else {
-    return (
-      <p>
-        To be done: {task} Duration: {duration}
-      </p>
-    );
-  }
+// ---Way 1: normal if else---
+
+// export default function ToDo({ task, isDone, duration }) {
+//   if (isDone) {
+//     return (
+//       <p>
+//         Done: {task} Duration: {duration}
+//       </p>
+//     );
+//   } else {
+//     return (
+//       <p>
+//         To be done: {task} Duration: {duration}
+//       </p>
+//     );
+//   }
+// }
+
+
+// ---Way 2: normal if return(else)---
+
+export default function ToDo({taskName, isDone ,duration}) {
+    if(isDone === 'true'){
+        return (
+            <p>Done: {taskName} Duration: {duration}</p>
+        )
+    }
+        return <p>To be done: {taskName} Duration: {duration}</p>
 }
